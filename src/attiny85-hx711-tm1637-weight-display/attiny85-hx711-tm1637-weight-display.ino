7fedb4431fbfd70afdbb60c10625d55e3835de44
Jason A. Cox <jason@jasonacox.com>
2020-07-18 12:04:12 -0700

    Updated setup calibration and animation

--- src/attiny85-hx711-tm1637-weight-display/attiny85-hx711-tm1637-weight-display.ino
+++ src/attiny85-hx711-tm1637-weight-display/attiny85-hx711-tm1637-weight-display.ino
@@ -7,93 +7,109 @@
 
   Components:
       ATiny85 Microcontroller
-      TM1636 4 Digit 7-Segment LED Display 
+      TM1636 4 Digit 7-Segment LED Display
       Load Cell
       HX711 Load Cell Amplifier (ADC)
       100uF Electrolytic Capacitor
       5V Power Supply
 
-  Callibration: 
+  Callibration:
       This sketch requires that you calibrate the load cell.  This involves the following steps:
       1) Run the sketch with DEBUG true (using a Arduino Uno or other microcontroller with serial)
-      2) Record the "HX711 reading" values with NO load on the scale - this is your "offset_value"
-      3) Use an trusted scale and weigh an object (grams) - record this value as your "known_value"
-      4) Place the object on the load cell and record the "HX711 reading" - this is "calibration_value"
-      5) Compute the ratio_value = (calibration_value - offset_value) / known_value
-      6) Edit the #defines below for CAL_RATIO and CAL_OFFSET
+      2) Record the "HX711 reading" values with NO load on the scale - this is your "CAL_OFFSET"
+      3) Use an trusted scale and weigh an object (grams) - record this value as your "KNOWN_VALUE"
+      4) Place the object on the load cell and record the "HX711 reading" - this is "CAL_VALUE"
+      5) Compute the CAL_RATIO value = (CAL_VALUE - CAL_OFFSET) / KNOWN_VALUE
+      6) Edit the #defines below
+
+      Example:
+        CAL_OFFSET = -148550
+        KNOWN_VALUE =  382.7186 g
+        CAL_VALUE = -107150
+        CAL_RATIO = (CAL_VALUE - CAL_OFFSET) / KNOWN_VALUE
+        CAL_RATIO = ((-107150) - (-148550 )) / (382.7186)
+        CAL_RATIO = 108.17
 
   Programming Notes:
       The TARE button uses PB0. If you use the Tiny AVR Programmer from Sparkfun
       it drives an LED on PB0 and once the sketch is uploaded, the TTiny will read PB0 as LOW
-      and assume you wish to TARE the scale. You will need to remove the the chip from the 
+      and assume you wish to TARE the scale. You will need to remove the the chip from the
       programmer after uploading to get it to work correctly in the circuit.
 
   Function:
       On start the circuit will read the last TARE value from EEPROM and display the the
       current weight. Press and hold the TARE button and the current weight value will be
       recorded in EEPROM and subtracted from the current reading to "Zero" out the scale.
-  
+
 */
-#include "EEPROM.h"
-#include "HX711.h"
-#include "segment.h"
+
+// Includes
+#include "EEPROM.h"   // Used to store tare values in EEPROM
+#include "segment.h"  // Animations for Display
 
 // Include TM1637TinyDisplay Library https://github.com/jasonacox/TM1637TinyDisplay
 #include <Arduino.h>
 #include <TM1637TinyDisplay.h>
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 1;
-const int LOADCELL_SCK_PIN = 2;
+// Include HX711 Arduino Library https://github.com/bogde/HX711
+#include <HX711.h>
 
-// Pins definitions for TM1637 and can be changed to other ports
-#define CLK 3
-#define DIO 4
+/*
+    CONFIGURATION SECTION - Update these values
+*/
+// Arduino Pin settings - UPDATE this section for your circuit
+const int LOADCELL_DOUT_PIN = 2;  // HX711 DT Pin
+const int LOADCELL_SCK_PIN = 3;   // HX711 SK Pin
+#define CLK 4                     // TM1637 Display CLK Pin
+#define DIO 5                     // TM1637 Display DIO Pin
+#define BUTTON 8                  // Tare Button - Pulled up / Push to low
+
+// Turn on debug mode for setting callibration
+#define DEBUG true
+
+// Calibration values - UPDATE this section for your load cell - see instructions above
+#define CAL_OFFSET -148550       // HX711 reading with no weight
+#define CAL_VALUE  -107150       // HX711 reading with item placed on scale
+#define KNOWN_VALUE    382.7186  // True weight of item placed on scale (e.g. in grams)
+
+/*
+   Calibration ratio will be computed based on the above
+   CAL_RATIO = (CAL_VALUE  -  CAL_OFFSET)  /  KNOWN_VALUE
+*/
+#define CAL_RATIO  ((CAL_VALUE) - (CAL_OFFSET)) / (KNOWN_VALUE)
+
+#define READ_SAMPLES 15   // Number of readings to average each cycle
+
+/*
+   SETUP
+*/
+// Initialize Global Classes 
 TM1637TinyDisplay display(CLK, DIO);
-
-// Pin for Tare button
-#define BUTTON 0
-
-// Debug mode for setting callibration
-#define DEBUG false
-
-// Calibration values
-#define CAL_RATIO 103.64
-#define CAL_OFFSET -123916
-
 HX711 scale;
 
 void setup() {
-
 #if DEBUG == true
   Serial.begin(38400);
 #endif
-
-  // Set Tare button
-  pinMode(BUTTON, INPUT);
-
+  pinMode(BUTTON, INPUT);       // Set Tare button
   display.setBrightness(0x0f); // set the brightness to 100 %
   delay(10);
 
   // Display Startup Sequence
   for (int x = 0; x < 5; x++) {
-    display.setSegments(SEG_ON);
+    display.showString(" On ");
     delay(200);
     display.clear();
     delay(100);
   }
-  for (int count = 0; count < 3; count++) {
-    for (int x = 0; x < 12; x++) {
-      display.setSegments(SEG_START[x]);
-      delay(10);
-    }
-  }
+  display.showAnimation(ANIMATION, FRAMES(ANIMATION), TIME_MS(10));
+  display.showString("Ender 3 Pro");
   display.showString("----");
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
   long offset = 0;
-  scale.set_scale(CAL_RATIO); // 98.11
+  scale.set_scale(CAL_RATIO);
   EEPROM.get(0, offset);
   if (offset == 0) {
     scale.set_offset(CAL_OFFSET); // default
@@ -101,19 +117,27 @@ void setup() {
   else {
     scale.set_offset(offset);
   }
-
 }
 
+/*
+   LOOP
+*/
 void loop() {
-
   if (scale.is_ready()) {
-    long reading = scale.read();
+    long reading = scale.read_average(READ_SAMPLES);
 #if DEBUG == true
     Serial.print("HX711 reading: ");
-    Serial.println(reading);
+    Serial.print(reading);
+    Serial.print(" ::: [ CAL_OFFSET: ");
+    Serial.print(scale.get_offset());
+    Serial.print(", CAL_RATIO: ");
+    Serial.print(scale.get_scale());
+    Serial.print(", Adjusted Value: ");
+    Serial.print((reading - scale.get_offset()) / scale.get_scale());
+    Serial.println(" ]");
 #endif
     // Display weight
-    display.showNumberDec((int)scale.get_units(10), false);
+    display.showNumber((int)((reading - scale.get_offset()) / scale.get_scale()));
 
     // Check to see if TARE button is pushed
     if (digitalRead(BUTTON) == LOW) {
